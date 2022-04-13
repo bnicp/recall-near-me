18ea2c9094fa14b4ab18fe873e9dc2576deecef1
bnicp <bnp415@gmail.com>
2022-04-12 21:55:26 -0400

    formatting using new framework w3.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,33 @@
 //https://open.fda.gov/apis/advanced-syntax/
-
 var eleContainer = document.getElementById("displayResults");
 var fetchButton = document.getElementById("search");
 var topicChoice = document.getElementById("displayNYT");
-
 function getApi() {
   //var requestUrl = 'https://api.github.com/repos/IBM/clai/issues?per_page=5';
   //alert("fetch");
-
   //construct url
   eleContainer.innerHTML = "";
-
   var url_1 = "https://api.fda.gov/food/enforcement.json?search=";
-
   var paramState = document.getElementById("srchState").value.trim();
   var paramCity = document.getElementById("srchCity").value.trim();
-  var paramFood = document.getElementById("srchFood").value.trim();
   var paramProd = document.getElementById("srchProd").value.trim();
   var paramFrmDt = document.getElementById("srchFrmDt").value.trim();
   var paramToDt = document.getElementById("srchToDt").value.trim();
   var urlArray = [];
-
   if (paramState != "") {
     paramState = paramState.split(" ").join("+");
     var url_2 = "state:" + paramState;
     urlArray.push(url_2);
   }
-
   if (paramCity != "") {
     paramCity = paramCity.split(" ").join("+");
     var url_3 = "+city:" + paramCity;
     urlArray.push(url_3);
   }
-
-  if (paramFood != "") {
-    paramFood = paramFood.split(" ").join("+");
-    var url_4 = "+reason_for_recall:" + paramFood;
-    urlArray.push(url_4);
-  }
-
   if (paramFrmDt != "" || paramToDt != "") {
     paramFrmDt = paramFrmDt.split("-").join("");
     paramToDt = paramToDt.split("-").join("");
     //alert(paramFrmDt);
-
     if (paramFrmDt != "" && paramToDt != "") {
       var url_5 = "report_date:" + "[" + paramFrmDt + "+TO+" + paramToDt + "]";
       urlArray.push(url_5);
@@ -53,13 +37,11 @@ function getApi() {
       // var url_5 = 'report_date:'+'['+paramToDt+']';
     }
   }
-
   if (paramProd != "") {
     paramProd = paramProd.split(" ").join("+");
     var url_6 = "+product_description:" + paramProd;
     urlArray.push(url_6);
   }
-
   for (var i = 0; i < urlArray.length; i++) {
     if (i == 0) {
       url_1 = url_1 + urlArray[i];
@@ -68,11 +50,9 @@ function getApi() {
     }
     //alert(url_1);
   }
-
   var requestUrl = url_1 + "&limit=5"; //+'report_date:[20220101+TO+20221231]&limit=5'
   // 'https://api.fda.gov/food/enforcement.json';
   //https://api.fda.gov/food/enforcement.json?search=reason_for_recall:peanut;
-
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -92,15 +72,64 @@ function getApi() {
           <p>Description:${results[i].product_description}</p>
           <p>Reason for recall:${results[i].reason_for_recall}</p>
           <p>Report Date:${results[i].report_date}</p></div>`; //+ htmlCreate;
-
           eleContainer.innerHTML += htmlCreate;
         }
       } else {
         eleContainer.innerHTML = "No Results Found";
       }
     });
+  var requestUrl1 =
+    "https://api.fda.gov/food/enforcement.json?search=reason_for_recall:milk&count=report_date";
+  var xValues = [];
+  var yValues = [];
+  fetch(requestUrl1)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var results = data.results;
+      for (var i = 0; i < 5; i++) {
+        xValues.push(results[i].time);
+        yValues.push(results[i].count);
+      }
+      ctx.innerHTML = "";
+      var barColors = ["red", "green", "blue", "orange", "brown"];
+      const myChart = new Chart(ctx, {
+        type: "doughnut",
+        data: {
+          labels: xValues,
+          datasets: [
+            {
+              data: yValues,
+              backgroundColor: barColors,
+              borderColor: [
+                "rgba(255, 99, 132, 1,0.3)",
+                "rgba(54, 162, 235, 1,0.3)",
+                "rgba(255, 206, 86, 1,0.3)",
+                "rgba(75, 192, 192, 1,0.3)",
+                "rgba(153, 102, 255, 1,0.3)",
+                "rgba(255, 159, 64, 1,0.3)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    });
+  var ctx = document.getElementById("myChart").getContext("2d");
+  //var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+  //var yValues = [55, 49, 44, 24, 15];
+  //console.log(xValues);
+  //console.log(yValues);
+  getMap();
 }
-
 function getNYTArticles(event) {
   topicChoice.innerHTML = "";
   var queryParam = event.target.textContent;
@@ -108,7 +137,6 @@ function getNYTArticles(event) {
   var nytAPIKey = "IpBihDlOE1r2nQVdTm0GsZyMB2Ba0BGQ";
   var nytRequestUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${queryParam}&api-key=${nytAPIKey}`;
   var nytArticle = "";
-
   fetch(nytRequestUrl)
     .then(function (response) {
       return response.json();
@@ -116,14 +144,11 @@ function getNYTArticles(event) {
     .then(function (results) {
       if (results.response.docs.length > 0) {
         var articleArray = results.response.docs;
-
         for (var i = 0; i < articleArray.length; i++) {
           var title = articleArray[i].headline.main;
-
           if (!title) {
             title = articleArray[i].headline.kicker;
           }
-
           if (articleArray[i].abstract) {
             var abstract = `<p>Abstract: ${articleArray[i].abstract}</p>`;
           } else {
@@ -146,18 +171,78 @@ function getNYTArticles(event) {
       }
     });
 }
-
-// Event listener for only the recall firm getting clicked on to search NYT articles
-$("#displayResults").on("click", ".recallFirm", getNYTArticles);
-
-fetchButton.addEventListener("click", getApi);
-
-//var foodFirm = document.createElement('h3');
-//var cityName= document.createElement('p');
-// alert(results[i].product_type+"-"+results[i].recalling_firm);
-//alert(results[i].city);
-
-//foodFirm.textContent = results[i].product_type+"-"+results[i].recalling_firm;
-//cityName.textContent ="City:"+results[i].city;
-//  issueContainer.appendChild(foodFirm);
-// issueContainer.appendChild(cityName);
+function getMap() {
+  var requestUrl1 =
+    "https://api.fda.gov/food/enforcement.json?count=state.exact";
+  var xValues = [];
+  var yValues = [];
+  fetch(requestUrl1)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var results = data.results;
+      //alert(results.length);
+      for (var i = 0; i < 25 /*results.length-1*/; i++) {
+        xValues.push(results[i].term);
+        yValues.push(results[i].count);
+      }
+      //var mapStates = 'AL,AK,AZ,AR,CA,CO,CT,DE,FL,GA,HI,ID,IL,IN,IA,KS,KY,LA,ME,MD,MA,MI,MN,MS,MO,MT,NE,NV,NH,NJ,NM,NY,NC,ND,OH,OK,OR,PA,RI,SC,SD,TN,TX,UT,VT,VA,WA,WV,WI,WY'.split(  ',');
+      var chartConfig = {
+        debug: true,
+        type: "map",
+        palette: {
+          pointValue: "{%zValue}",
+          colors: [
+            "#f7fcfd",
+            "#e5f5f9",
+            "#ccece6",
+            "#99d8c9",
+            "#66c2a4",
+            "#41ae76",
+            "#238b45",
+            "#006d2c",
+            "#00441b",
+          ],
+          //ranges: { min: 0, max: 1000, interval: 200 },
+          //  defaultRange_legendEntry_value: '$%min - $%max'
+        },
+        legend: {
+          //  title_label_text: 'Sales',
+          //  template: '%value %icon',
+          position: "top",
+        },
+        defaultPoint: {
+          label_text: "%stateCode",
+          tooltip: "<b>%name</b> <br/>Count: {%zValue}",
+        },
+        //defaultSeries_shape_padding: 0.02,
+        series: [{ id: "usMap", map: "us" }],
+      };
+      chartConfig.series[0].points = getRandomPoints();
+      var chart = JSC.chart("chartDiv", chartConfig);
+      var xNew = "";
+      var yNew = "";
+      function getRandomPoints() {
+        for (i = 0; i < xValues.length - 1; i++) {
+          xNew = xNew + "," + xValues[i];
+          yNew = yNew + "," + yValues[i];
+        }
+        xNew = xNew.split(",");
+        yNew = yNew.split(",");
+        return xNew.map(function (arrItem, index) {
+          return { map: "US." + arrItem, z: yNew[index] };
+        });
+      }
+    });
+}
+// Event listener for whole page
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("recallFirm")) {
+    getNYTArticles(event);
+  }
+  if (event.target.id === "search") {
+    event.preventDefault();
+    getApi();
+  }
+});
