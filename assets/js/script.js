0a7bd4c89162d4982cd0acc282a6e22630764ea9
Heather Noyes <heatherknoyes@gmail.com>
2022-04-14 20:26:01 -0400

    Got rid of reset as W3 framework takes care of the reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -264,6 +264,16 @@ function getMap() {
     });
 }
 
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
 // Event listener for whole page
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("recallFirm")) {
