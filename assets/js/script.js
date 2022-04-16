6007af6d12c5ccbb2ccd17da98b76fe379fc1e4f
Heather Noyes <heatherknoyes@gmail.com>
2022-04-16 11:51:28 -0400

    Pulling in most recent commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,52 +20,27 @@ function getMap() {
     })
     .then(function (data) {
       var results = data.results;
-      //alert(results.length);
-      for (var i = 0; i < 25 /*results.length-1*/; i++) {
+      for (var i = 0; i < 25; i++) {
         xValues.push(results[i].term);
         yValues.push(results[i].count);
       }
 
-      //var mapStates = 'AL,AK,AZ,AR,CA,CO,CT,DE,FL,GA,HI,ID,IL,IN,IA,KS,KY,LA,ME,MD,MA,MI,MN,MS,MO,MT,NE,NV,NH,NJ,NM,NY,NC,ND,OH,OK,OR,PA,RI,SC,SD,TN,TX,UT,VT,VA,WA,WV,WI,WY'.split(  ',');
       var chartConfig = {
         debug: true,
         type: "map",
-        palette: {
-          pointValue: "{%zValue}",
-          colors: [
-            "#f7fcfd",
-            "#e5f5f9",
-            "#ccece6",
-            "#99d8c9",
-            "#66c2a4",
-            "#41ae76",
-            "#238b45",
-            "#006d2c",
-            "#00441b",
-          ],
-          //ranges: { min: 0, max: 1000, interval: 200 },
-          //  defaultRange_legendEntry_value: '$%min - $%max'
-        },
-        legend: {
-          //  title_label_text: 'Sales',
-          //  template: '%value %icon',
-          position: "bottom",
-        },
-
         defaultPoint: {
           label_text: "%stateCode",
-          tooltip: "<b>%name</b> <br/>Count: {%zValue}",
+          tooltip: "<b>%name</b> <br/>Total Recalls: {%zValue}",
         },
         //defaultSeries_shape_padding: 0.02,
-        series: [{ id: "usMap", map: "us" }],
+        series: [{ color: "#D0CE7D", points: getRandomPoints() }],
       };
 
-      chartConfig.series[0].points = getRandomPoints();
       var chart = JSC.chart("chartDiv", chartConfig);
       var xNew = "";
       var yNew = "";
       function getRandomPoints() {
-        for (i = 0; i < xValues.length - 1; i++) {
+        for (i = 0; i < xValues.length - 2; i++) {
           xNew = xNew + "," + xValues[i];
           yNew = yNew + "," + yValues[i];
         }
@@ -74,6 +49,12 @@ function getMap() {
         yNew = yNew.split(",");
 
         return xNew.map(function (arrItem, index) {
+          if (arrItem === "undefined") {
+            arrItem = "M";
+          }
+          if (yNew[index] === "undefined") {
+            yNew[index] = 0;
+          }
           return { map: "US." + arrItem, z: yNew[index] };
         });
       }
@@ -92,4 +73,109 @@ document.addEventListener("click", function (event) {
   }
 });
 
-window.onload = getMap();
+//document.getElementById("chartModal").innerHTML = "Chart goes here";
+var varModal = "";
+
+document.addEventListener("mouseover", function (e) {
+  e.stopPropagation();
+  if (e.target.innerHTML === "defective") {
+    varModal = "defective";
+    // alert(varModal);
+    getChart(varModal);
+  } else if (e.target.innerHTML === "adulterated") {
+    varModal = "adulterated";
+    getChart(varModal);
+  } else if (e.target.innerHTML === "contaminated") {
+    varModal = "contaminated";
+    getChart(varModal);
+  } else if (e.target.innerHTML === "misbranded") {
+    varModal = "misbranded";
+    getChart(varModal);
+  } else if (e.target.innerHTML === "mislabeled") {
+    varModal = "mislabeled";
+    getChart(varModal);
+  } else {
+  }
+});
+
+function getChart(varModal) {
+  var requestUrl1 =
+    //"https://api.fda.gov/food/enforcement.json?search=reason_for_recall:milk&count=report_date&sort=report_date:desc&limit=10";
+    "https://api.fda.gov/food/enforcement.json?search=reason_for_recall:" +
+    varModal +
+    "&count=city.exact&sort=report_date:desc&limit=5";
+
+  var xValues = [];
+  var yValues = [];
+  fetch(requestUrl1)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var results = data.results;
+      for (var i = 0; i < 3; i++) {
+        xValues.push(results[i].term);
+        //alert(xValues);
+        yValues.push(results[i].count);
+      }
+
+      new Chart("chartModal", {
+        type: "bar",
+        data: {
+          labels: xValues,
+          datasets: [
+            {
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.3)",
+                "rgba(54, 162, 235, 0.3)",
+                "rgba(255, 206, 86, 0.3)",
+                "rgba(75, 192, 192, 0.3)",
+                "rgba(153, 102, 255, 0.3)",
+                "rgba(255, 159, 64, 0.3)",
+              ],
+              data: yValues,
+            },
+          ],
+        },
+
+        options: {
+          legend: { display: false },
+          title: {
+            display: true,
+            text: varModal,
+            responsive: true,
+          },
+          scales: {
+            xAxes: [
+              {
+                gridLines: {
+                  color: "rgba(0, 0, 0, 0)",
+                },
+              },
+            ],
+            yAxes: [
+              {
+                gridLines: {
+                  color: "rgba(0, 0, 0, 0)",
+                },
+              },
+            ],
+            yAxes: [
+              {
+                display: false,
+              },
+            ],
+          },
+          plugins: {
+            datalabels: {
+              display: true,
+              align: "center",
+              anchor: "center",
+            },
+          },
+        },
+      });
+    });
+}
+
+init();
